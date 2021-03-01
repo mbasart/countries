cb975f4165ec43dc5c6bc29f63176eafa76a3642
mbasart <mbd2797@gmail.com>
2021-03-01 23:37:44 +0100

    ex 2.14 done

--- src/components/Country.js
+++ src/components/Country.js
@@ -2,20 +2,54 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const Country = ({filteredCountries}) => {
-    const country = filteredCountries[0]
-    return (
-        <div>
-            <h1>{country.name}</h1>
-            <p>capital {country.capital}</p>
-            <p>population {country.population}</p>
-            <h3>languages</h3>
-            <ul>
-                {country.languages.map(lang => <li key={lang.iso639_2}>{lang.name}</li>)}
-            </ul>
-            <img src={country.flag} alt='Country flag' width="150" />
-        </div>
-    )
+    const api_key = process.env.REACT_APP_API_KEY
+// variable api_key has now the value set in startup
 
+    const country = filteredCountries[0]
+
+    const [ weather, setWeather ] = useState('')
+
+    useEffect(() => {
+        axios
+        .get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}&units=m`)
+        .then(response => {
+            setWeather(response.data.current)
+        })
+    }, [api_key, country.capital])
+
+
+    if (weather === ''){
+        return (
+            <div>
+                <h1>{country.name}</h1>
+                <p>capital {country.capital}</p>
+                <p>population {country.population}</p>
+                <h3>languages</h3>
+                <ul>
+                    {country.languages.map(lang => <li key={lang.iso639_2}>{lang.name}</li>)}
+                </ul>
+                <img src={country.flag} alt='Country flag' width="150" />
+            </div>
+        )
+
+    } else {
+        return (
+            <div>
+                <h1>{country.name}</h1>
+                <p>capital {country.capital}</p>
+                <p>population {country.population}</p>
+                <h3>languages</h3>
+                <ul>
+                    {country.languages.map(lang => <li key={lang.iso639_2}>{lang.name}</li>)}
+                </ul>
+                <img src={country.flag} alt='Country flag' width="150" />
+                <h1>Weather in {country.capital}</h1>
+                <p><b>temperature:</b> {weather.temparature} Celsius</p>
+                <img src={weather.weather_icons} alt='Weather incon' />
+                <p><b>wind: </b>{weather.wind_speed} km/h direction {weather.wind_dir}</p>
+            </div>
+        )
+    }
 }
 
 export default Country
